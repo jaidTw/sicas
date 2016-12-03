f7d29bddd0e1ee55ac9eff8230cde8cba5bd5231
Jesse Huang <jessehuang2222@gmail.com>
2016-12-03 17:43:37 +0800

    Fininsh basic pass1 features.

---
+++ sicas.py
@@ -0,0 +1,209 @@
+#!/usr/bin/python
+
+import argparse
+import os
+from sicxe import *
+
+
+class AssembleError(BaseException):
+    pass
+
+class Line:
+    def __init__(self, assembly, lineno):
+        self.assembly = assembly
+        self.code = ""
+        self.lineno = lineno
+        self.fmt = 0
+    
+    def __str__(self):
+        return self.assembly
+
+    def tokenize(self):
+        return self.assembly.split()
+
+class Program:
+    def __init__(self, source):
+        self.source = os.path.basename(source)
+        self.name = ''
+        self.start_addr = 0x0
+        self.started = False
+        self.LOCCTR = 0
+        self.lineno = 0
+        self.content = list(Line(line.strip('\n'), lineno) for lineno, line in enumerate(open(source, "r").readlines(), 1))
+        self.symtab = PRELOAD_SYMTAB.copy()
+
+    def error(self, msg):
+        print("\n%s:%s" % (self.source, str(self.lineno)) + "  " + str(self.content[self.lineno - 1]))
+        print("Error : " + msg + '\n')
+        raise AssembleError
+
+    def assemble(self):
+        for line in self.content:
+            if line.assembly[0] == '.':
+                continue
+            print("%04X\t %s" % (self.LOCCTR, line))
+            tokens = line.tokenize()
+
+            self.lineno = line.lineno
+            if has_directives(self, tokens):
+                continue
+            elif has_instructions(self, tokens):
+                continue
+            else:
+                program.error("Except a directive, opcde or label.")
+
+def handler_START(program, tokens):
+    if "START" in tokens:
+        # validate format
+        if program.started:
+            program.error("Multiple START detected.")
+        elif tokens[0] == "START":
+            program.error("Must specify a name for program before START.")
+        elif tokens[2] == "START":
+            program.error("Multiple tokens were specified before START.")
+        if "CHECK PROGRAM NAME FORMAT" != 0:
+            pass
+
+        program.name = tokens[0]
+        try:
+            program.start_addr = int(tokens[2], 16)
+        except ValueError:
+           program.error("%s is an invalid value for starting address (hexadecimal is required)." % tokens[2])
+        program.started = True
+
+def handler_END(program_inf, tokens):
+    pass
+
+def handler_BYTE(program_inf, tokens):
+    if tokens[0] == "BYTE":
+        program.error("Must specify a label for the allocated space.")
+    elif tokens[2] == "BYTE":
+        program.error("Multiple label were specified for BYTE.")
+    elif len(tokens) < 3:
+        program.error("Requires an value for BYTE.")
+
+    "CHECK LABEL NAME"
+    value = tokens[2]
+    if value[0] == 'C':
+        "CHECK QUOTION MARKS"
+        program.symtab[tokens[0]] = program.LOCCTR
+        program.LOCCTR += len(tokens[2][2:-1])
+    elif value[0] == 'X':
+        try:
+            "CHECK QUOTION MARKS"
+            program.symtab[tokens[0]] = program.LOCCTR
+            program.LOCCTR += 1
+        except ValueError:
+            program.error("The \"X\" requires a hex value, but %s is not." % value[2:-1])
+
+def handler_WORD(program_inf, tokens):
+    pass
+
+def handler_RESW(program, tokens):
+    if tokens[0] == "RESW":
+        program.error("Must specify a label for the allocated space.")
+    elif tokens[2] == "RESW":
+        program.error("Multiple label were specified for RESW.")
+    elif len(tokens) < 3:
+        program.error("Requires an length for RESW.")
+
+    "CHECK LABEL NAME"
+    "LENGTH IS DECIMAL"
+    program.symtab[tokens[0]] = program.LOCCTR
+    program.LOCCTR += int(tokens[2]) * 3
+
+def handler_RESB(program_inf, tokens):
+    if tokens[0] == "RESB":
+        program.error("Must specify a label for the allocated space.")
+    elif tokens[2] == "RESB":
+        program.error("Multiple label were specified for RESB.")
+    elif len(tokens) < 3:
+        program.error("Requires an length for RESB.")
+
+    "CHECK LABEL NAME"
+    "LENGTH IS DECIMAL"
+    program.symtab[tokens[0]] = program.LOCCTR
+    program.LOCCTR += int(tokens[2])
+
+def handler_BASE(program_inf, tokens):
+    pass
+
+DIRTAB = {
+    "START" : handler_START,
+    "END"   : handler_END,
+    "BYTE"  : handler_BYTE,
+    "WORD"  : handler_WORD,
+    "RESB"  : handler_RESB,
+    "RESW"  : handler_RESW,
+    "BASE"  : handler_BASE,
+}
+
+def has_directives(program, tokens):
+    for token in tokens:
+        if token in DIRTAB:
+            DIRTAB[token](program, tokens)
+            return True
+    return False
+
+def has_instructions(program, tokens):
+    for idx, token in enumerate(tokens):
+        prefix = ""
+        inst = ""
+        fmt = 0
+        if token in OPTAB:
+            inst = token
+
+            if idx == 1:
+                "CHECK LABEL"
+                program.symtab[tokens[0]] = program.LOCCTR
+        elif token[1:] in OPTAB:
+            prefix = token[0]
+            inst = token[1:]
+            if prefix == '+' and not (OPTAB[inst].inf & FORMAT4):
+                program.error("%s does not support format 4." % inst)
+            else:
+                fmt = 4
+
+            if idx == 1:
+                "CHECK LABEL"
+                program.symtab[tokens[0]] = program.LOCCTR
+        else:
+            continue
+
+        if fmt != 4:
+            mask = OPTAB[inst].inf & 0xE0
+            while mask != 0x8:
+                mask >>= 1
+                fmt += 1
+            fmt = (5 - fmt)
+        program.LOCCTR += fmt
+
+        return True
+    return False
+
+def has_labels(program_inf, tokens):
+    pass
+
+
+if __name__ == "__main__":
+    # Parse the arguments
+    parser = argparse.ArgumentParser(description="A Python SIC/XE Assembler")
+    parser.add_argument('-o', help='the output file.', default='a.out')
+    parser.add_argument('--listing', help='generate assembly listing.')
+    parser.add_argument('input', nargs='+', help='the source assembly file(s).')
+    args = parser.parse_args()
+    input_files = args.input
+
+    print("SIC/XE Assembler")
+
+    # Open files in the list
+    for file_name in input_files:
+        program = Program(file_name)
+        try:
+            print("\nStart assembly %s ..." % program.source)
+            program.assemble()
+            print("Done.")
+            print(program.symtab)
+        except AssembleError:
+            print("Assemble failed.")
+            continue
---
+++ sicxe.py
@@ -0,0 +1,106 @@
+BYTESIZE = 8
+
+MODE_C = 0x1
+MODE_F = 0x2
+MODE_X = 0x4
+MODE_P = 0x8
+
+FORMAT4 = 0x10
+FORMAT3 = 0x20
+FORAMT2 = 0x40
+FORMAT1 = 0x80
+
+class instruction:
+    def __init__(self, opcode, fmt, mode=""):
+        self.opcode = opcode
+        self.inf = 0x10 << (4 - fmt)
+        if self.inf & FORMAT3:
+            self.inf |= FORMAT4
+        if "P" in mode:
+            self.inf |= MODE_P
+        if "X" in mode:
+            self.inf |= MODE_X
+        if "F" in mode:
+            self.inf |= MODE_F
+        if "C" in mode:
+            self.inf |= MODE_C
+
+    def __str__(self):
+        return "instruction(%x, %s)" % (self.opcode, bin(self.inf))
+
+    def __repr__(self):
+        return str(self)
+
+PRELOAD_SYMTAB = {
+    "A" : 0,
+    "X" : 1,
+    "L" : 2,
+    "B" : 3,
+    "S" : 4,
+    "T" : 5,
+    "F" : 6,
+    "PC" : 8,
+    "SW" : 9
+}
+
+OPTAB = {
+    "ADD"    : instruction(0x18, 3),
+    "ADDF"   : instruction(0x58, 3, "XF"),
+    "ADDR"   : instruction(0x90, 2, "X"),
+    "AND"    : instruction(0x40, 3),
+    "CLEAR"  : instruction(0xB4, 2, "X"),
+    "COMP"   : instruction(0x28, 3),
+    "COMPF"  : instruction(0x88, 3, "XFC"),
+    "COMPR"  : instruction(0xA0, 2, "XFC"),
+    "DIV"    : instruction(0x24, 3),
+    "DIVF"   : instruction(0x64, 3, "XF"),
+    "DIVR"   : instruction(0x9C, 2, "X"),
+    "FIX"    : instruction(0xC4, 1, "XF"),
+    "FLOAT"  : instruction(0xC0, 1, "XF"),
+    "HIO"    : instruction(0xF4, 1, "PX"),
+    "J"      : instruction(0x3C, 3),
+    "JEQ"    : instruction(0x30, 3),
+    "JGT"    : instruction(0x34, 3),
+    "JLT"    : instruction(0x38, 3),
+    "JSUB"   : instruction(0x48, 3),
+    "LDA"    : instruction(0x00, 3),
+    "LDB"    : instruction(0x68, 3, "X"),
+    "LDCH"   : instruction(0x50, 3),
+    "LDF"    : instruction(0x70, 3, "XF"),
+    "LDL"    : instruction(0x08, 3),
+    "LDS"    : instruction(0x6C, 3, "X"),
+    "LDT"    : instruction(0x74, 3, "X"),
+    "LDX"    : instruction(0x04, 3),
+    "LPS"    : instruction(0xD0, 3, "PX"),
+    "MUL"    : instruction(0x20, 3),
+    "MULF"   : instruction(0x60, 3, "XF"),
+    "MULR"   : instruction(0x98, 2, "X"),
+    "NORM"   : instruction(0xC8, 1, "XF"),
+    "OR"     : instruction(0x44, 3),
+    "RD"     : instruction(0xD8, 3, "P"),
+    "RMO"    : instruction(0xAC, 2, "X"),
+    "RSUB"   : instruction(0x4C, 3),
+    "SHIFTL" : instruction(0xA4, 2, "X"),
+    "SHIFTR" : instruction(0xA8, 2, "X"),
+    "SIO"    : instruction(0xF0, 1, "PX"),
+    "SSK"    : instruction(0xEC, 3, "PX"),
+    "STA"    : instruction(0x0C, 3),
+    "STB"    : instruction(0x78, 3, "X"),
+    "STCH"   : instruction(0x54, 3),
+    "STF"    : instruction(0x80, 3, "X"),
+    "STI"    : instruction(0xD4, 3, "PX"),
+    "STL"    : instruction(0x14, 3),
+    "STS"    : instruction(0x7C, 3, "X"),
+    "STSW"   : instruction(0xE8, 3, "P"),
+    "STT"    : instruction(0x84, 3, "X"),
+    "STX"    : instruction(0x10, 3),
+    "SUB"    : instruction(0x1C, 3),
+    "SUBF"   : instruction(0x5C, 3, "XF"),
+    "SUBR"   : instruction(0x94, 2, "X"),
+    "SVC"    : instruction(0xB0, 2, "X"),
+    "TD"     : instruction(0xE0, 3, "PC"),
+    "TIO"    : instruction(0xF8, 1, "PXC"),
+    "TIX"    : instruction(0x2C, 3, "C"),
+    "TIXR"   : instruction(0xB8, 2, "XC"),
+    "WD"     : instruction(0xDC, 3, "P"),
+}
